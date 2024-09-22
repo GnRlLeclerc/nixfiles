18c881b9dfac6e9858aae72a5ce0b339ee37f497
Thibaut <thibaut2saivre@gmail.com>
2024-09-22 16:17:32 +0200

    Wrap the firefox native messaging host in a bash script

--- home-manager/modules/theme/items/firefox.nix
+++ home-manager/modules/theme/items/firefox.nix
@@ -30,12 +30,25 @@ in
       firefox-native-base16
     ];
 
+    # Create a wrapper script to run the native messaging host (else it does not work)
+    home.file.".mozilla/native-messaging-scripts/firefox_native_base16" = {
+      text = ''
+        #!/usr/bin/env bash
+
+        trap 'kill -SIGTERM $native_pid' SIGTERM
+        ${pkgs.firefox-native-base16}/bin/firefox-native-base16 &
+        native_pid=$!
+        wait $native_pid
+      '';
+      executable = true;
+    };
+
     # Native messaging manifest
     home.file.".mozilla/native-messaging-hosts/firefox_native_base16.json".text = builtins.toJSON {
       name = "firefox_native_base16";
       description = "Native messaging application to dynamically communicate base16 colorschemes to Firefox";
       type = "stdio";
-      path = "${pkgs.firefox-native-base16}/bin/firefox-native-base16";
+      path = "${config.home.homeDirectory}/.mozilla/native-messaging-scripts/firefox_native_base16";
       allowed_extensions = [ "dynamic_base16@gnrl_leclerc.org" ];
     };
   };

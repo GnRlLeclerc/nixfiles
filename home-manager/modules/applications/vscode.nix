4a3a995a21333d6c00e9aa367b3cb267dd353398
Thibaut <thibaut2saivre@gmail.com>
2025-03-23 16:05:59 +0100

    Back to usual vscode (blurry fixed + mutable extensions dir)

--- home-manager/modules/applications/vscode.nix
+++ home-manager/modules/applications/vscode.nix
@@ -1,14 +1,5 @@
 # VSCode configuration
-{ pkgs, ... }:
-{
-  # Manage VSCode the usual way
-  programs.vscode.package =
-    (pkgs.vscode.override {
-      # Add this flag to fix blurry vscode on wayland when using ozone & fractional scaling
-      # See https://github.com/microsoft/vscode/issues/203303#issuecomment-1913443702
-      commandLineArgs = [ "--disable-features=WaylandFractionalScaleV1" ];
-    }).fhs;
-
+_: {
   # Symlink the keybindings and settings in a readonly way
   xdg.configFile."Code/User" = {
     source = ../../dotfiles/vscode;

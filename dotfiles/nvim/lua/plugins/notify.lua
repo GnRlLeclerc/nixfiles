66d9754970fc9d92da048c4c930e8c823e90d825
Thibaut <thibaut2saivre@gmail.com>
2025-08-02 17:40:38 +0200

    fix notify background color

--- dotfiles/nvim/lua/plugins/notify.lua
+++ dotfiles/nvim/lua/plugins/notify.lua
@@ -2,6 +2,7 @@ return {
   'rcarriga/nvim-notify',
   config = function()
     require('notify').setup({
+      background_colour = require('catppuccin.palettes').get_palette('dynamic').base,
       enabled = false,
       stages = 'fade',
       timeout = 5000,

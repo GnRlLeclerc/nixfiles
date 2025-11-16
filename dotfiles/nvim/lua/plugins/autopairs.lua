1fe858d8303afd5ec60503f472983331d581a12b
Thibaut <thibaut2saivre@gmail.com>
2025-11-16 18:03:48 +0100

    tune autopair rules how I like it

--- dotfiles/nvim/lua/plugins/autopairs.lua
+++ dotfiles/nvim/lua/plugins/autopairs.lua
@@ -9,14 +9,40 @@ return {
     local Rule = require('nvim-autopairs.rule')
     local cond = require('nvim-autopairs.conds')
 
+    local ignored_next_char = [=[[%w%%%'%[%"%.%`%$]]=]
+
+    -- NOTE: additional autopair rules for markdown and typst
+    -- Quotes-like: only when enclosed in whitespaces so that
+    -- it basically triggers only before typing a new word,
+    -- and not when trying to enclose an existing word
+    -- or close an existing quote.
+
     -- Add * and _ matching for typst files
     npairs.add_rules({
-      Rule('*', '*', 'typst'):with_move(cond.before_regex('\\*')),
-      Rule('$', '$', 'typst'):with_move(cond.before_regex('$')),
-      Rule('_', '_', 'typst'):with_move(function(opts)
-        -- Need custom rule because regex does not work in _<word>_ situation
-        return vim.api.nvim_get_current_line():sub(opts.col, opts.col) == '_'
-      end),
+      Rule('*', '*', 'typst')
+        :with_move(cond.after_regex('\\*', 1))
+        :with_pair(cond.not_after_regex(ignored_next_char))
+        :with_pair(cond.not_before_regex(ignored_next_char)),
+      Rule('$', '$', 'typst')
+        :with_move(cond.after_regex('$', 1))
+        :with_pair(cond.not_after_regex(ignored_next_char))
+        :with_pair(cond.not_before_regex(ignored_next_char)),
+      Rule('_', '_', 'typst')
+        :with_move(cond.after_regex('_', 1))
+        :with_pair(cond.not_after_regex(ignored_next_char))
+        :with_pair(cond.not_before_regex(ignored_next_char)),
+    })
+
+    -- Add ** and _ matching for markdown files
+    npairs.add_rules({
+      Rule('**', '**', 'markdown')
+        :with_move(cond.after_regex('\\*\\*', 2))
+        :with_pair(cond.not_after_regex(ignored_next_char))
+        :with_pair(cond.not_before_regex(ignored_next_char)),
+      Rule('_', '_', 'markdown')
+        :with_move(cond.after_regex('_', 1))
+        :with_pair(cond.not_after_regex(ignored_next_char))
+        :with_pair(cond.not_before_regex(ignored_next_char)),
     })
   end,
 }
